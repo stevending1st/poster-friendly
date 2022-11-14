8542c4710a696180082868f80dcefd4b880057bc
stevending1st <stevending1st@163.com>
2022-11-14 21:13:00 +0800

    feat: init vue ts vite typescript element-plus

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>Home</h1>
+
+  <h2>{{ msg }}</h2>
+
+  <div class="card">
+    <el-button @click="count++">count is {{ count }}</el-button>
+  </div>
+</template>
+
+<style scoped>
+</style>
