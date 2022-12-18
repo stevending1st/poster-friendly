0484364b744766e840c2f7eb4af280ccada56b23
stevending1st <stevending1st@163.com>
2022-12-19 00:54:06 +0800

    refactor!: change vue app to vitepress

--- website/components.d.ts
+++ website/components.d.ts
@@ -7,6 +7,12 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    CommonHeader: typeof import('./.vitepress/components/common-header.vue')['default']
+    ConfigurationForm: typeof import('./.vitepress/components/configuration-form.vue')['default']
+    CreateButton: typeof import('./.vitepress/components/create-button.vue')['default']
+    FormItems: typeof import('./.vitepress/components/form-items.vue')['default']
+    PreviewCom: typeof import('./.vitepress/components/preview-com.vue')['default']
+    RenderForm: typeof import('./.vitepress/components/render-form.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
