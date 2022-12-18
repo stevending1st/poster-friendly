7e950eee3e3460f08de7b5b2d5c420df3382e147
stevending1st <stevending1st@163.com>
2022-12-19 00:15:22 +0800

    feat: add vitepress

---
+++ website/.vitepress/config.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'Poster Friendly',
+  description:
+    'A complete solution for GitHub (Gitee) Issue and Discussion templates.',
+  lang: 'en-US',
+  cleanUrls: 'with-subfolders',
+  cacheDir: '../.cache',
+  themeConfig: {
+    nav: [],
+  },
+};
---
+++ website/vite.config.ts
@@ -0,0 +1,54 @@
+import { resolve } from 'path';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import Unocss from 'unocss/vite';
+import { presetUno, presetAttributify, presetIcons, presetMini } from 'unocss';
+
+export default {
+  server: {
+    hmr: {
+      overlay: false,
+    },
+    fs: {
+      allow: [resolve(__dirname, '..')],
+    },
+  },
+  ssr: { noExternal: ['element-plus'] },
+  plugins: [
+    Components({
+      dirs: ['.vitepress/components'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [ElementPlusResolver()],
+    }),
+    AutoImport({
+      imports: ['vue'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [ElementPlusResolver()],
+    }),
+    Unocss({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons(),
+        presetMini({
+          theme: {
+            breakponts: {
+              xs: '320px',
+              sm: '640px',
+              md: '960px',
+              lg: '1280px',
+            },
+          },
+        }),
+      ],
+      rules: [
+        ['text-decoration-none', { 'text-decoration': 'none' }],
+        [
+          /^font-size-(\d+)$/,
+          ([, d]) => ({ 'font-size': `${d}rem !important` }),
+        ],
+      ],
+    }),
+  ],
+};
