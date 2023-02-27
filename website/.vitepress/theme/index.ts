41a11ec21f2ba312696fad5be270ac07aa3da980
stevending1st <stevending1st@163.com>
2023-02-27 16:08:36 +0800

    fix(theme): add global component and ID_INJECTION_KEY

--- website/.vitepress/theme/index.ts
+++ website/.vitepress/theme/index.ts
@@ -1,7 +1,29 @@
 import DefaultTheme from 'vitepress/theme';
-
+import {
+  ID_INJECTION_KEY,
+  ElCard,
+  ElInput,
+  ElForm,
+  ElFormItem,
+} from 'element-plus';
 import 'uno.css';
+
+import RenderForm from './components/render-form.vue';
+import TemplateInfo from './components/template-info.vue';
 
 export default {
   ...DefaultTheme,
+  enhanceApp(ctx) {
+    DefaultTheme.enhanceApp(ctx);
+    ctx.app.provide(ID_INJECTION_KEY, {
+      prefix: 1024,
+      current: 0,
+    });
+    ctx.app.component('ElCard', ElCard);
+    ctx.app.component('ElInput', ElInput);
+    ctx.app.component('ElForm', ElForm);
+    ctx.app.component('ElFormItem', ElFormItem);
+    ctx.app.component('RenderForm', RenderForm);
+    ctx.app.component('TemplateInfo', TemplateInfo);
+  },
 };
