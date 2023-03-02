9385936d9b4fbdbbfad9b256e8fe0414683ee377
Steven Ding <stevending1st@163.com>
2023-03-02 21:39:21 +0800

    fix(TemplateInfo): remove Teleport tag (#3)

--- website/.vitepress/theme/components/template-info.vue
+++ website/.vitepress/theme/components/template-info.vue
@@ -20,36 +20,30 @@ const lastUpdated = useTimeAgo(new Date(updateTime));
 </script>
 
 <template>
-  <Teleport to="body">
-    <div class="grid grid-cols-[100px_auto] gap-2 text-sm my-4 items-start">
-      <div opacity="50">Last Changed</div>
-      <div flex="~ gap-1 wrap">{{ lastUpdated }}</div>
-    </div>
+  <div class="grid grid-cols-[100px_auto] gap-2 text-sm my-4 items-start">
+    <div opacity="50">Last Changed</div>
+    <div flex="~ gap-1 wrap">{{ lastUpdated }}</div>
+  </div>
 
-    <div
-      class="grid grid-cols-[100px_auto] gap-2 text-sm mt-4 mb-4 items-start"
-    >
-      <div opacity="50">File Size</div>
-      <div flex="~ gap-1 wrap">{{ fileSize }}</div>
-    </div>
+  <div class="grid grid-cols-[100px_auto] gap-2 text-sm mt-4 mb-4 items-start">
+    <div opacity="50">File Size</div>
+    <div flex="~ gap-1 wrap">{{ fileSize }}</div>
+  </div>
 
-    <div
-      class="grid grid-cols-[100px_auto] gap-2 text-sm mt-4 mb-4 items-start"
-    >
-      <div opacity="50">GitHub URL</div>
-      <div flex="~ gap-1 wrap">
-        {{ configFileGitHubURL }}
-      </div>
+  <div class="grid grid-cols-[100px_auto] gap-2 text-sm mt-4 mb-4 items-start">
+    <div opacity="50">GitHub URL</div>
+    <div flex="~ gap-1 wrap">
+      {{ configFileGitHubURL }}
     </div>
+  </div>
 
-    <div
-      class="grid grid-cols-[100px_auto] gap-2 text-sm mt-4 mb-4 items-start"
-      v-if="configFileGiteeURL"
-    >
-      <div opacity="50">Gitee URL</div>
-      <div flex="~ gap-1 wrap">
-        {{ configFileGiteeURL }}
-      </div>
+  <div
+    class="grid grid-cols-[100px_auto] gap-2 text-sm mt-4 mb-4 items-start"
+    v-if="configFileGiteeURL"
+  >
+    <div opacity="50">Gitee URL</div>
+    <div flex="~ gap-1 wrap">
+      {{ configFileGiteeURL }}
     </div>
-  </Teleport>
+  </div>
 </template>
